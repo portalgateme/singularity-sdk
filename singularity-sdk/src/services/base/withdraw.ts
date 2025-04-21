bc42a4f428731b260e353a3a0138a6ec39b48122
Lư Chí Tâm <chitam.lu97@gmail.com>
2025-04-21 15:04:46 +0700

    format code

--- singularity-sdk/src/services/base/withdraw.ts
+++ singularity-sdk/src/services/base/withdraw.ts
@@ -1,126 +1,139 @@
-import { Note, PartialNote, WithdrawProofResult, generateWithdrawProof } from "@thesingularitynetwork/darkpool-v1-proof";
-import { Action, relayerPathConfig } from "../../config/config";
-import { WithdrawRelayerRequest } from "../../entities/relayerRequestTypes";
-import { hexlify32 } from "../../utils/util";
-import { BaseRelayerContext, BaseRelayerResult, BaseRelayerService } from "../BaseService";
-import { getMerklePathAndRoot } from "../merkletree";
-import { Relayer } from "../../entities/relayer";
-import { DarkpoolError } from "../../entities";
-import { DarkPool } from "../../darkpool";
+import {
+  Note,
+  PartialNote,
+  WithdrawProofResult,
+  generateWithdrawProof
+} from '@thesingularitynetwork/darkpool-v1-proof';
+import { Action, relayerPathConfig } from '../../config/config';
+import { WithdrawRelayerRequest } from '../../entities/relayerRequestTypes';
+import { hexlify32 } from '../../utils/util';
+import { BaseRelayerContext, BaseRelayerResult, BaseRelayerService } from '../BaseService';
+import { getMerklePathAndRoot } from '../merkletree';
+import { Relayer } from '../../entities/relayer';
+import { DarkpoolError } from '../../entities';
+import { DarkPool } from '../../darkpool';
 
 class WithdrawContext extends BaseRelayerContext {
-    private _note?: Note;
-    private _recipient?: string;
-    private _proof?: WithdrawProofResult;
+  private _note?: Note;
+  private _recipient?: string;
+  private _proof?: WithdrawProofResult;
 
-    constructor(relayer: Relayer, signature: string) {
-        super(relayer, signature);
-    }
+  constructor(relayer: Relayer, signature: string) {
+    super(relayer, signature);
+  }
 
-    set note(note: Note | undefined) {
-        this._note = note;
-    }
+  set note(note: Note | undefined) {
+    this._note = note;
+  }
 
-    get note(): Note | undefined {
-        return this._note;
-    }
+  get note(): Note | undefined {
+    return this._note;
+  }
 
-    set recipient(recipient: string | undefined) {
-        this._recipient = recipient;
-    }
+  set recipient(recipient: string | undefined) {
+    this._recipient = recipient;
+  }
 
-    get recipient(): string | undefined {
-        return this._recipient;
-    }
+  get recipient(): string | undefined {
+    return this._recipient;
+  }
 
-    set proof(proof: WithdrawProofResult | undefined) {
-        this._proof = proof;
-    }
+  set proof(proof: WithdrawProofResult | undefined) {
+    this._proof = proof;
+  }
 
-    get proof(): WithdrawProofResult | undefined {
-        return this._proof;
-    }
+  get proof(): WithdrawProofResult | undefined {
+    return this._proof;
+  }
 }
 
-
-
 export class WithdrawService extends BaseRelayerService<WithdrawContext, WithdrawRelayerRequest, BaseRelayerResult> {
-    constructor(_darkPool: DarkPool) {
-        super(_darkPool);
+  constructor(_darkPool: DarkPool) {
+    super(_darkPool);
+  }
+
+  public async prepare(
+    note: Note,
+    recipient: string,
+    signature: string
+  ): Promise<{ context: WithdrawContext; outPartialNotes: PartialNote[] }> {
+    const context = new WithdrawContext(this._darkPool.getRelayer(), signature);
+    context.note = note;
+    context.recipient = recipient;
+    return { context, outPartialNotes: [] };
+  }
+
+  public async generateProof(context: WithdrawContext): Promise<void> {
+    if (!context || !context.note || !context.recipient || !context.signature) {
+      throw new DarkpoolError('Invalid context');
     }
 
-    public async prepare(note: Note, recipient: string, signature: string): Promise<{ context: WithdrawContext, outPartialNotes: PartialNote[] }> {
+    const path = await getMerklePathAndRoot(context.note.note, this._darkPool);
+    context.merkleRoot = path.root;
 
-        const context = new WithdrawContext(this._darkPool.getRelayer(), signature);
-        context.note = note;
-        context.recipient = recipient;
-        return { context, outPartialNotes: [] };
+    const proof = await generateWithdrawProof({
+      note: context.note,
+      address: context.recipient,
+      merkleRoot: path.root,
+      merklePath: path.path,
+      merkleIndex: path.index,
+      relayer: context.relayer.relayerAddress,
+      signedMessage: context.signature,
+      options: this._darkPool.proofOptions
+    });
+    context.proof = proof;
+  }
+
+  public async getRelayerRequest(context: WithdrawContext): Promise<WithdrawRelayerRequest> {
+    if (
+      !context ||
+      !context.note ||
+      !context.signature ||
+      !context.merkleRoot ||
+      !context.recipient ||
+      !context.proof
+    ) {
+      throw new DarkpoolError('Invalid context');
     }
 
-    public async generateProof(context: WithdrawContext): Promise<void> {
-        if (!context || !context.note || !context.recipient || !context.signature) {
-            throw new DarkpoolError("Invalid context");
-        }
+    const withdrawRelayerRequest: WithdrawRelayerRequest = {
+      proof: context.proof.proof.proof,
+      asset: context.note.asset,
+      merkleRoot: context.merkleRoot,
+      nullifier: context.proof.nullifier,
+      recipient: context.recipient,
+      relayer: context.relayer.relayerAddress,
+      amount: hexlify32(context.note.amount),
+      refund: hexlify32(0n),
+      verifierArgs: context.proof.proof.verifyInputs
+    };
 
-        const path = await getMerklePathAndRoot(context.note.note, this._darkPool);
-        context.merkleRoot = path.root;
+    return withdrawRelayerRequest;
+  }
 
-        const proof = await generateWithdrawProof({
-            note: context.note,
-            address: context.recipient,
-            merkleRoot: path.root,
-            merklePath: path.path,
-            merkleIndex: path.index,
-            relayer: context.relayer.relayerAddress,
-            signedMessage: context.signature,
-            options: this._darkPool.proofOptions
-        });
-        context.proof = proof;
+  public getRelayerPath(): string {
+    return relayerPathConfig[Action.WITHDRAW];
+  }
+
+  //eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public async postExecute(context: WithdrawContext): Promise<BaseRelayerResult> {
+    console.log(context.tx);
+    return { txHash: context.tx! };
+  }
+
+  public getRelayerContractCallParameters(context: WithdrawContext) {
+    if (!context || !context.note || !context.recipient || !context.signature || !context.proof) {
+      throw new DarkpoolError('Invalid context');
     }
 
-    public async getRelayerRequest(context: WithdrawContext): Promise<WithdrawRelayerRequest> {
-        if (!context || !context.note || !context.signature || !context.merkleRoot || !context.recipient || !context.proof) {
-            throw new DarkpoolError("Invalid context");
-        }
-
-        const withdrawRelayerRequest: WithdrawRelayerRequest = {
-            proof: context.proof.proof.proof,
-            asset: context.note.asset,
-            merkleRoot: context.merkleRoot,
-            nullifier: context.proof.nullifier,
-            recipient: context.recipient,
-            relayer: context.relayer.relayerAddress,
-            amount: hexlify32(context.note.amount),
-            refund: hexlify32(0n),
-            verifierArgs: context.proof.proof.verifyInputs,
-        };
-
-        return withdrawRelayerRequest;
-    }
-
-    public getRelayerPath(): string {
-        return relayerPathConfig[Action.WITHDRAW];
-    }
-
-    //eslint-disable-next-line @typescript-eslint/no-unused-vars
-    public async postExecute(context: WithdrawContext): Promise<BaseRelayerResult> {
-        console.log(context.tx);
-        return { txHash: context.tx! };
-    }
-
-    public getRelayerContractCallParameters(context: WithdrawContext) {
-        if (!context || !context.note || !context.recipient || !context.signature || !context.proof) {
-            throw new DarkpoolError("Invalid context");
-        }
-
-        return {
-            proof: context.proof.proof.proof,
-            asset: context.note.asset,
-            merkleRoot: context.merkleRoot,
-            nullifier: context.proof.nullifier,
-            recipient: context.recipient,
-            relayer: context.relayer.relayerAddress,
-            amount: hexlify32(context.note.amount),
-        }
-    }
+    return {
+      proof: context.proof.proof.proof,
+      asset: context.note.asset,
+      merkleRoot: context.merkleRoot,
+      nullifier: context.proof.nullifier,
+      recipient: context.recipient,
+      relayer: context.relayer.relayerAddress,
+      amount: hexlify32(context.note.amount)
+    };
+  }
 }
--- singularity-sdk/src/services/darkpool/createTakerOrder.ts
+++ singularity-sdk/src/services/darkpool/createTakerOrder.ts
@@ -1,129 +1,144 @@
-import { createNote, DarkPoolTakerCreateOrderProofResult, DarkPoolTakerSwapMessage, generateDarkPoolTakerCreateOrderProof, Note, generateDarkPoolTakerSwapMessage } from "@thesingularitynetwork/darkpool-v1-proof";
-import { ethers } from "ethers";
+import {
+  createNote,
+  DarkPoolTakerCreateOrderProofResult,
+  DarkPoolTakerSwapMessage,
+  generateDarkPoolTakerCreateOrderProof,
+  Note,
+  generateDarkPoolTakerSwapMessage
+} from '@thesingularitynetwork/darkpool-v1-proof';
+import { ethers } from 'ethers';
 import DarkpoolSwapAssetManagerAbi from '../../abis/DarkPoolSwapAssetManager.json';
-import { DarkPool } from "../../darkpool";
-import { DarkpoolError } from "../../entities";
-import { BaseContext, BaseContractService } from "../BaseService";
-import { getMerklePathAndRoot } from "../merkletree";
-
+import { DarkPool } from '../../darkpool';
+import { DarkpoolError } from '../../entities';
+import { BaseContext, BaseContractService } from '../BaseService';
+import { getMerklePathAndRoot } from '../merkletree';
 
 class CreateTakerOrderContext extends BaseContext {
-    private _outgoingNote?: Note;
-    private _incomingNote?: Note;
-    private _proof?: DarkPoolTakerCreateOrderProofResult;
-    private _feeAmount?: bigint;
-    private _takerSwapMessage?: DarkPoolTakerSwapMessage;
+  private _outgoingNote?: Note;
+  private _incomingNote?: Note;
+  private _proof?: DarkPoolTakerCreateOrderProofResult;
+  private _feeAmount?: bigint;
+  private _takerSwapMessage?: DarkPoolTakerSwapMessage;
 
-    constructor(signature: string) {
-        super(signature);
-    }
+  constructor(signature: string) {
+    super(signature);
+  }
 
-    set incomingNote(note: Note | undefined) {
-        this._incomingNote = note;
-    }
+  set incomingNote(note: Note | undefined) {
+    this._incomingNote = note;
+  }
 
-    get incomingNote(): Note | undefined {
-        return this._incomingNote;
-    }
+  get incomingNote(): Note | undefined {
+    return this._incomingNote;
+  }
 
-    set outgoingNote(note: Note | undefined) {
-        this._outgoingNote = note;
-    }
+  set outgoingNote(note: Note | undefined) {
+    this._outgoingNote = note;
+  }
 
-    get outgoingNote(): Note | undefined {
-        return this._outgoingNote;
-    }
+  get outgoingNote(): Note | undefined {
+    return this._outgoingNote;
+  }
 
-    set proof(proof: DarkPoolTakerCreateOrderProofResult | undefined) {
-        this._proof = proof;
-    }
+  set proof(proof: DarkPoolTakerCreateOrderProofResult | undefined) {
+    this._proof = proof;
+  }
 
-    get proof(): DarkPoolTakerCreateOrderProofResult | undefined {
-        return this._proof;
-    }
+  get proof(): DarkPoolTakerCreateOrderProofResult | undefined {
+    return this._proof;
+  }
 
-    set feeAmount(feeAmount: bigint | undefined) {
-        this._feeAmount = feeAmount;
-    }
+  set feeAmount(feeAmount: bigint | undefined) {
+    this._feeAmount = feeAmount;
+  }
 
-    get feeAmount(): bigint | undefined {
-        return this._feeAmount;
-    }
+  get feeAmount(): bigint | undefined {
+    return this._feeAmount;
+  }
 
-    set takerSwapMessage(takerSwapMessage: DarkPoolTakerSwapMessage | undefined) {
-        this._takerSwapMessage = takerSwapMessage;
-    }
+  set takerSwapMessage(takerSwapMessage: DarkPoolTakerSwapMessage | undefined) {
+    this._takerSwapMessage = takerSwapMessage;
+  }
 
-    get takerSwapMessage(): DarkPoolTakerSwapMessage | undefined {
-        return this._takerSwapMessage;
-    }
+  get takerSwapMessage(): DarkPoolTakerSwapMessage | undefined {
+    return this._takerSwapMessage;
+  }
 }
 
 export class CreateTakerOrderService extends BaseContractService<CreateTakerOrderContext> {
+  constructor(_darkPool: DarkPool) {
+    super(_darkPool);
+  }
 
-    constructor(_darkPool: DarkPool) {
-        super(_darkPool);
+  private async getFeeAmount(): Promise<bigint> {
+    return 0n; //FIXME
+  }
+
+  public async prepare(
+    outgoingNote: Note,
+    incomingAsset: string,
+    incomingAmount: bigint,
+    signature: string
+  ): Promise<{ context: CreateTakerOrderContext; outNotes: Note[] }> {
+    const context = new CreateTakerOrderContext(signature);
+    context.outgoingNote = outgoingNote;
+    const feeAmount = await this.getFeeAmount();
+    if (feeAmount >= incomingAmount) {
+      throw new DarkpoolError('Fee amount is greater than out amount');
+    }
+    context.feeAmount = feeAmount;
+    const incomingNote = await createNote(incomingAsset, incomingAmount - feeAmount, signature);
+    context.incomingNote = incomingNote;
+    context.takerSwapMessage = await generateDarkPoolTakerSwapMessage({
+      outNote: outgoingNote,
+      inNote: incomingNote,
+      feeAsset: incomingNote.asset,
+      feeAmount: feeAmount,
+      signedMessage: signature
+    });
+    return { context, outNotes: [incomingNote] };
+  }
+
+  public async generateProof(context: CreateTakerOrderContext): Promise<void> {
+    if (!context || !context.incomingNote || !context.outgoingNote) {
+      throw new DarkpoolError('Invalid context');
     }
 
-    private async getFeeAmount(): Promise<bigint> {
-        return 0n;//FIXME
+    const merklePath = await getMerklePathAndRoot(context.outgoingNote.note, this._darkPool);
+    context.merkleRoot = merklePath.root;
+
+    const proof = await generateDarkPoolTakerCreateOrderProof({
+      merkleRoot: merklePath.root,
+      merklePath: merklePath.path,
+      merkleIndex: merklePath.index,
+      outNote: context.outgoingNote,
+      inNote: context.incomingNote,
+      feeAmount: context.feeAmount ?? 0n,
+      signedMessage: context.signature,
+      options: this._darkPool.proofOptions
+    });
+    context.proof = proof;
+  }
+
+  public async execute(context: CreateTakerOrderContext): Promise<string> {
+    if (!context || !context.incomingNote || !context.proof || !context.merkleRoot) {
+      throw new DarkpoolError('Invalid context');
     }
 
-    public async prepare(outgoingNote: Note, incomingAsset: string, incomingAmount: bigint, signature: string): Promise<{ context: CreateTakerOrderContext, outNotes: Note[] }> {
-        const context = new CreateTakerOrderContext(signature);
-        context.outgoingNote = outgoingNote;
-        const feeAmount = await this.getFeeAmount();
-        if (feeAmount >= incomingAmount) {
-            throw new DarkpoolError("Fee amount is greater than out amount");
-        }
-        context.feeAmount = feeAmount;
-        const incomingNote = await createNote(incomingAsset, incomingAmount - feeAmount, signature);
-        context.incomingNote = incomingNote;
-        context.takerSwapMessage = await generateDarkPoolTakerSwapMessage({
-            outNote: outgoingNote,
-            inNote: incomingNote,
-            feeAsset: incomingNote.asset,
-            feeAmount: feeAmount,
-            signedMessage: signature,
-        });
-        return { context, outNotes: [incomingNote] };
-    }
-
-    public async generateProof(context: CreateTakerOrderContext): Promise<void> {
-        if (!context || !context.incomingNote || !context.outgoingNote) {
-            throw new DarkpoolError("Invalid context");
-        }
-
-        const merklePath = await getMerklePathAndRoot(context.outgoingNote.note, this._darkPool);
-        context.merkleRoot = merklePath.root;
-
-        const proof = await generateDarkPoolTakerCreateOrderProof({
-            merkleRoot: merklePath.root,
-            merklePath: merklePath.path,
-            merkleIndex: merklePath.index,
-            outNote: context.outgoingNote,
-            inNote: context.incomingNote,
-            feeAmount: context.feeAmount ?? 0n,
-            signedMessage: context.signature,
-            options: this._darkPool.proofOptions
-        });
-        context.proof = proof;
-    }
-
-    public async execute(context: CreateTakerOrderContext): Promise<string> {
-        if (!context || !context.incomingNote || !context.proof || !context.merkleRoot) {
-            throw new DarkpoolError("Invalid context");
-        }
-
-        const contract = new ethers.Contract(this._darkPool.contracts.darkpoolSwapAssetManager, DarkpoolSwapAssetManagerAbi.abi, this._darkPool.signer);
-        const tx = await contract.takerCreateOrder(
-            context.merkleRoot,
-            context.proof.outNullifier,
-            context.incomingNote.asset,
-            context.proof.feeAmount,
-            context.proof.inNote,
-            context.proof.inNoteFooter,
-            context.proof.proof.proof);
-        return tx.hash;
-    }
+    const contract = new ethers.Contract(
+      this._darkPool.contracts.darkpoolSwapAssetManager,
+      DarkpoolSwapAssetManagerAbi.abi,
+      this._darkPool.signer
+    );
+    const tx = await contract.takerCreateOrder(
+      context.merkleRoot,
+      context.proof.outNullifier,
+      context.incomingNote.asset,
+      context.proof.feeAmount,
+      context.proof.inNote,
+      context.proof.inNoteFooter,
+      context.proof.proof.proof
+    );
+    return tx.hash;
+  }
 }
